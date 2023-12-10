95cf8659e5a68ec213e28b890d2ad4b94a0f8bca
Roy de Wildt <roydewildt@gmail.com>
2023-12-10 09:10:17 +0100

    day 7

--- day7/puzzle_test.go
+++ day7/puzzle_test.go
@@ -1,1 +1,47 @@
 package day7_test
+
+import (
+	_ "embed"
+	"testing"
+
+	"github.com/scrot/aoc2023"
+	"github.com/scrot/aoc2023/day7"
+)
+
+//go:embed input.txt
+var input []byte
+
+const example = `32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483`
+
+const edge = `12345 100
+54321 1
+12543 10`
+
+func TestDay7(t *testing.T) {
+	cs := []struct {
+		name    string
+		part    int
+		version aoc2023.Solver
+		input   []byte
+		want    int
+	}{
+		{"p1Edge", 1, day7.V1{}, []byte(edge), 123},
+		{"p1Example", 1, day7.V1{}, []byte(example), 6440},
+		{"p1Input", 1, day7.V1{}, input, 253603890},
+		{"p2Example", 2, day7.V1{}, []byte(example), 5905},
+		{"p2Input", 2, day7.V1{}, input, 0},
+	}
+
+	for _, c := range cs {
+		t.Run(c.name, func(t *testing.T) {
+			got, _ := c.version.Solve(c.input, c.part)
+			if got != c.want {
+				t.Errorf("want %v got %v", c.want, got)
+			}
+		})
+	}
+}
--- day7/puzzle_v1.go
+++ day7/puzzle_v1.go
@@ -1,7 +1,158 @@
 package day7
 
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"slices"
+	"sort"
+	"strconv"
+	"strings"
+)
+
 type V1 struct{}
 
+type hand struct {
+	cards [5]int
+	bid   int
+}
+
+type hands struct {
+	part int
+	arr  []hand
+}
+
+// addHand takes a input line and converts
+// it into a hand with cards and a bid
+func (hs *hands) addHand(line string) {
+	l := strings.Split(line, " ")
+
+	var h hand
+	for i, r := range l[0] {
+		h.cards[i] = toValue(r, hs.part)
+	}
+
+	h.bid, _ = strconv.Atoi(l[1])
+
+	hs.arr = append(hs.arr, h)
+}
+
+// implementation of sort.Interface
+func (hs *hands) Len() int {
+	return len(hs.arr)
+}
+
+// implementation of sort.Interface
+func (hs *hands) Less(i, j int) bool {
+	// hi, hj := highestCard(hs.arr[i]), highestCard(hs.arr[j])
+	si, sj := strength(hs.arr[i], hs.part), strength(hs.arr[j], hs.part)
+
+	switch {
+	case si < sj:
+		return true
+	// case si == 0 && sj == 0 && hi != hj:
+	// 	return hi < hj
+	case si == sj:
+		for k := 0; k < 5; k++ {
+			ci, cj := hs.arr[i].cards[k], hs.arr[j].cards[k]
+			if ci != cj {
+				return ci < cj
+			}
+		}
+		return true
+	default:
+		return false
+	}
+}
+
+// implementation of sort.Interface
+func (hs *hands) Swap(i, j int) {
+	hs.arr[i], hs.arr[j] = hs.arr[j], hs.arr[i]
+}
+
 func (V1) Solve(input []byte, part int) (int, error) {
-	return 0, nil
+	r := bytes.NewReader(input)
+	s := bufio.NewScanner(r)
+
+	hs := hands{part: part}
+	for s.Scan() {
+		hs.addHand(s.Text())
+	}
+
+	sort.Sort(&hs)
+
+	var winnings int
+	for i, h := range hs.arr {
+		winnings += (i + 1) * h.bid
+		fmt.Printf("hand %v strength %v\n", h, strength(h, part))
+	}
+
+	return winnings, nil
+}
+
+// strenght returns a ranking of card combinations
+// with the highest is a 5-of-a-kind and weakest a high-card
+func strength(h hand, part int) int {
+	combo := make(map[int]int)
+	for _, c := range h.cards {
+		combo[c]++
+	}
+
+	var counts []int
+	for k, v := range combo {
+		if part == 1 {
+			counts = append(counts, v)
+		}
+		if part == 2 && k != 1 {
+			counts = append(counts, v)
+		}
+	}
+	slices.Sort(counts)
+
+	if part == 2 {
+		if len(counts) == 0 {
+			return 6
+		}
+		counts[len(counts)-1] += combo[1]
+	}
+
+	switch hi := counts[len(counts)-1]; hi {
+	case 5:
+		return 6
+	case 4:
+		return 5
+	case 3:
+		if counts[len(counts)-2] == 2 {
+			return 4
+		}
+		return 3
+	case 2:
+		if counts[len(counts)-2] == 2 {
+			return 2
+		}
+		return 1
+	default:
+		return 0
+	}
+}
+
+// toValue turns card symbol into integer
+func toValue(card rune, part int) int {
+	switch card {
+	case 'A':
+		return 14
+	case 'K':
+		return 13
+	case 'Q':
+		return 12
+	case 'J':
+		if part == 2 {
+			return 1
+		}
+		return 11
+	case 'T':
+		return 10
+	default:
+		return int(card - '0')
+	}
 }
